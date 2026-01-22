4f2ed8cbb53aeb2ea203171b6d66bdcde252808f
ty <1206036143@qq.com>
2026-01-22 18:29:24 +0800

    33

--- demo/TestCode2.java
+++ demo/TestCode2.java
@@ -8,7 +8,7 @@ public class TestCode2 {
 
     // 手机号正则表达式（简单版）
     private static final Pattern PHONE_PATTERN = Pattern.compile("^1[3-9]\\d{9}$");
-    // 邮箱正则表达式（简单版）
+    // 邮箱正则表达式（简单版） 
     private static final Pattern EMAIL_PATTERN = Pattern.compile("^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$");
 
     /**
